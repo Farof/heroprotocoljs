84909feb267b22c08148b70b166a8ce791f53899
Mathieu Merdy <gfarof@gmail.com>
2016-01-28 19:55:50 +0100

    build info

--- data.js
+++ data.js
@@ -50,3 +50,37 @@ exports.heroes = [
 exports.mounts = [];
 
 exports.maps = [];
+
+// see to complete http://us.battle.net/heroes/en/search?k=Patch%20Notes&f=article
+exports.builds = {
+  39951: {
+    live: {
+      link: 'http://us.battle.net/heroes/en/blog/19993879/heroes-of-the-storm-patch-notes-january-12-2016-1-12-2016'
+      patch: '15.5',
+      from: '2016-01-12',
+      to: '2016-01-27'
+    }
+  }''
+  40987: {
+    live: {
+      link: 'http://us.battle.net/heroes/en/blog/19996517/heroes-of-the-storm-balance-update-notes-january-20-2016-1-20-2016'
+      patch: '15.6',
+      from: '2016-01-20',
+      to: '2016-01-27'
+    }
+  },
+  40322: {
+    live: {
+      link: 'http://us.battle.net/heroes/en/blog/20021838',
+      patch: '15.7',
+      from: '2016-01-27'
+    }
+  },
+  40336: {
+    ptr: {
+      link: 'http://us.battle.net/heroes/en/blog/19998381',
+      patch: '16.0',
+      from: '2016-01-26'
+    }
+  }
+};
