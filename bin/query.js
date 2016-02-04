4c596c17fcb0ae7c55307073eada1e13b2d914c3
Mathieu Merdy <gfarof@gmail.com>
2016-02-04 02:48:48 +0100

    filter arrays

--- bin/query.js
+++ bin/query.js
@@ -32,18 +32,63 @@ if (args.f === 'header') {
   data = archive.get(`replay.${args.f}`);
 }
 
-if (data) {
-  const parts = args.q.split('.');
-  let value;
 
-  try {
-    value = parts.reduce((obj, key) => obj[key], data);
-  } catch (err) {
-    
+const opquery = /(.*?)(=|!=)(.*)/;
+function filter(ar, filter) {
+  const match = filter.match(opquery);
+  if (!match) {
+    console.log('Unsupported filter:', filter);
+    process.exit(1);
   }
 
+  const left = match[1].trim(), operand = match[2].trim(), right = match[3].trim();
+
+  return ar.filter(item => {
+    const value = find(item, left);
+    if (operand === '=') {
+      return value == right;
+    } else if (operand === '!=') {
+      return value != right;
+    }
+  });
+}
+
+const subquery = /(?:\[(.*?)\])/;
+function find(obj, query, map) {
+  const match = query.match(subquery);
+  let value = obj, part, sub, next;
+
+  if (match) {
+    part = query.slice(0, match.index);
+    sub = match[1];
+    next = query.slice(match.index + sub.length + 3);
+  } else if(query.indexOf('.') > -1) {
+    part = query.slice(0, query.indexOf('.'));
+    next = query.slice(query.indexOf('.') + 1)
+  } else {
+    part = query;
+  }
+
+  value = map ? value : value[part];
+  if (sub) {
+    value = filter(value, sub);
+  }
+
+  if (next) {
+    if (sub) value = value.map(item => {
+      return find(item, next);
+    });
+    else value = find(value, next);
+  }
+
+  return value;
+}
+
+if (data) {
+  const value = find(data, args.q, args.f.includes('events'));
+
   if (value) {
-    console.log(args.q, '=', value);
+    console.log(args.q, '=', Array.from(new Set(value)).sort());
   } else {
     console.log('Unknown query path: ', args.q);
   }
